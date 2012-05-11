b269f5f0280b2a6c1b0269324fdbb2af40fcdd15
Duncan Grazier <itsmeduncan@gmail.com>
2012-05-11 10:48:23 -0400

    Only store successes after success

--- pullable.rb
+++ pullable.rb
@@ -9,15 +9,17 @@ raise ArgumentError.new("Please pass a root directoy!") if root.nil?
 
 Dir.foreach(root) do |directory|
   if File.directory?(directory)
-    pulled << directory
-
     FileUtils.cd(directory)
+
     output = `git pull`
+
     FileUtils.cd(root)
 
     unless $?.success?
       failed[directory.to_sym] = output
     else
+      pulled << directory
+
       print '.'
       $stdout.flush
     end
